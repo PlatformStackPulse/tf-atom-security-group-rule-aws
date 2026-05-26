9d09bcdc3660e2e506400bc8dc17fc736748acca
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 23:14:30 +0100

    feat: implement tf-atom-security-group-rule-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "ingress_rule_id" {
+  description = "ID of the ingress rule"
+  value       = try(aws_vpc_security_group_ingress_rule.ingress[0].id, null)
+}
+
+output "egress_rule_id" {
+  description = "ID of the egress rule"
+  value       = try(aws_vpc_security_group_egress_rule.egress[0].id, null)
 }
